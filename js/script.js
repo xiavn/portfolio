5732b8256747254386b78d4119dd8d2707634cb3
Lianne Turnbull <lianneturnbull@gmail.com>
2016-07-02 13:29:28 +0100

    Style modal

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var portfolio  =[
 		"name": "Rumour Generator",
 		"snippet": "Easily expandable generator that produces random rumours for use in storytelling and rpg's.",
 		"link": "http://codepen.io/xiavn/pen/VLNQQM",
-		"image": ["rumour.png", "rumour2.png", "rumour3.png"],
+		"image": "rumour.png",
 		"tags": ["regex", "javascript"],
 		"description": "This project was created as part of FreeCodeCamp, the task was to create a random quote generator. I decided I wanted to try and expand on the concept of a quote generator, and instead build something that could be useful when running tabletop roleplaying games to randomly generate rumours that could be given to the players to add flavour and inspire adventure. When creating the generator one of my main goals was to make it modular and easy to expand to allow for a greater variety of generated responses. I utilised Regular Expressions and a series of objects to allow new words to be added in categories that can then be randomly picked and filled into gaps in the core rumour template. This allows you to write a basic rumour and specify specific categories to be used when filling in the blanks - such as ay type of location, or only rural locations, male pronouns or female, and plural forms of animals."
 	},
@@ -11,7 +11,7 @@ var portfolio  =[
 		"name": "Local Weather",
 		"snippet": "Webpage that looks up the local weather for the user and displays various forecasts for the future.",
 		"link": "http://codepen.io/xiavn/pen/NGqxoJ",
-		"image": [],
+		"image": "weather.png",
 		"tags": ["ajax", "api", "local storage"],
 		"description": "This was a project set by FreeCodeCamp with the goal of creating a page that would utilise the user's location to display the current weather for them through the OpenWeatherMap API. It should also allow them to view the results in either celcius or farenheit. I decided to expand this further by displaying a wider variety of forecasts, including the current, next 12 hours, and next three days and to allow the user to search for and change the location set. To build on top of this, in order to limit the number of repeat requests to the API, the page uses local storage to save results, and only refreshes if 30 minutes has past, or the user looks for a new city. It also stores the results of searches, allowing the user to recall previous searches they have already done."
 	}];
@@ -25,7 +25,7 @@ function createPortfolio(data) {
 		var name = project.name,
 			snippet = project.snippet,
 			tags = project.tags[0],
-			image = project.image[0];
+			image = project.image;
 
 		//Add additional tags
 		for (var i=1; i<project.tags.length; i++) {
@@ -34,7 +34,7 @@ function createPortfolio(data) {
 
 		portfolioDisplay+= "<div class='portfolio-square'><div><img src='../img/" +
 		image +
-		"'></img></div><div class='overlay'><h4>" +
+		"'></div><div class='overlay'><h4>" +
 		name +
 		"</h4><p>" +
 		 snippet +
@@ -47,5 +47,5 @@ function createPortfolio(data) {
 }
 	
 $(document).ready(function(){
-	createPortfolio(portfolio);
+	//createPortfolio(portfolio);
 });
